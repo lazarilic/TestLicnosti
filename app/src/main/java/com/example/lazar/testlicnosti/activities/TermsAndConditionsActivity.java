f62ab73bf2d8aba57f0435accee3d26244460f08
Lazar Ilic <lazarilic9@gmail.com>
2018-03-12 20:42:37 +0100

    Terms and conditions implemented (#2)

--- app/src/main/java/com/example/lazar/testlicnosti/activities/TermsAndConditionsActivity.java
+++ app/src/main/java/com/example/lazar/testlicnosti/activities/TermsAndConditionsActivity.java
@@ -2,21 +2,44 @@ package com.example.lazar.testlicnosti.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.lazar.testlicnosti.R;
 
 /**
  * Activity responsible for showing Terms and Conditions
  */
-public class TermsAndConditionsActivity extends Activity {
+public class TermsAndConditionsActivity extends Activity{
+
+    Button acceptButton, returnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_and_conditions);
+        initViews();
+        setOnClickListeners();
+    }
 
-//        findViewById(R.id.iconBack).setOnClickListener(view -> {
-//            finish();
-//        });
+    private void initViews() {
+        acceptButton = findViewById(R.id.acceptButton);
+        returnButton = findViewById(R.id.returnButton);
+    }
+    private void setOnClickListeners() {
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                finish();
+            }
+        });
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
